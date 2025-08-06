231cf8e84cc5ee79d3353a1704c2e1f5794ad46c
Muhammad Safwan P K <62184316+sfwnmuhd@users.noreply.github.com>
2025-08-06 10:21:15 +0300

    Updated Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -251,7 +251,7 @@ const Header = () => {
           initial={{ opacity: 0, y: 30 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.8, delay: 0.8 }}
-          className='mt-16 pb-16'
+          className='mt-1 pb-16'
         >
           <div className='text-center mb-8'>
             <p className='text-sm font-semibold text-gray-500 uppercase tracking-wider'>
